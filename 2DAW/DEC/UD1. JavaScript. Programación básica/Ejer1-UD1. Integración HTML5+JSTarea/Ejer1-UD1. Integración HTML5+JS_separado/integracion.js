d480151419893ce43b1c27ffb1d46234c0342a99
ceciliacortess <ceciliacortesvelert@gmail.com>
2023-09-14 10:35:09 +0200

    Añadir carpetas y ejercicio 1

---
+++ 2DAW/DEC/UD1. JavaScript. Programación básica/Ejer1-UD1. Integración HTML5+JSTarea/Ejer1-UD1. Integración HTML5+JS_separado/integracion.js
@@ -0,0 +1,5 @@
+numero1 = parseInt(prompt("Introduce el numero 1: "));
+numero2 = parseInt(prompt("Introduce el numero 2: "));
+let resultado = numero1 + numero2;
+alert("El resultado es: " + resultado);
+    
