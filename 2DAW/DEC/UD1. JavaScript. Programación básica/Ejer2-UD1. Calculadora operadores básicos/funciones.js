263661bd3edfe87c68485bd0ba8f48c84e95dfdc
ceciliacortess <cecorvel2004@gmail.com>
2023-09-19 08:43:03 +0200

    Ejercicios 2 y 3

---
+++ 2DAW/DEC/UD1. JavaScript. Programación básica/Ejer2-UD1. Calculadora operadores básicos/funciones.js
@@ -0,0 +1,8 @@
+numero1 = parseInt(prompt("Introduce el numero 1: "));
+numero2 = parseInt(prompt("Introduce el numero 2: "));
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+alert("El resultado de la resta es: " + resta);
+alert("El resultado de la multiplicacion es: " + multiplicacion); 
+alert("El resultado de la division es: " + division);
