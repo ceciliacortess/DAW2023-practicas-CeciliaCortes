29b96ec08fc7a832965b839fced2623c6637d5bf
ceciliacortess <cecorvel2004@gmail.com>
2023-09-19 09:02:44 +0200

    Ejercicio 4 y 5

---
+++ 2DAW/DEC/UD1. JavaScript. Programación básica/Ejer4-UD1. Funciones flecha. Comparación de números/comparar.js
@@ -0,0 +1,4 @@
+num1 = 6;
+num2 = 3;
+let numeroMayor = (num1 < num2) ? () => console.log('El ' + num1 + ' es menor al ' + num2) : () => console.log('El ' + num1 + ' es mayor al ' + num2);
+numeroMayor();
---
+++ 2DAW/DEC/UD1. JavaScript. Programación básica/Ejer5-UD1. Funciones flecha. Número mayor/numeroMayor.js
@@ -0,0 +1,3 @@
+num = 6;
+let mayor = (num < 20) ? () => console.log('El ' + num + ' es menor al 20') : () => console.log('El ' + num + ' es mayor al 20');
+mayor();
